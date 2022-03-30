6f1dec908136c728dbee6dcf7003970bb8f21d93
Thobeka Sithole <59669252+Thobeka-coder@users.noreply.github.com>
2022-03-30 11:30:55 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCddDRpjj4EX317TBukT77jmM28Zh3xg60",
+  apiKey: "******",
   authDomain: "bitpay-accommodation.firebaseapp.com",
   projectId: "bitpay-accommodation",
   storageBucket: "bitpay-accommodation.appspot.com",
-  messagingSenderId: "339451240093",
-  appId: "1:339451240093:web:d7aa50a60047e22c493e74",
-  measurementId: "G-S5M8RQ29WH"
+  messagingSenderId: "******",
+  appId: "1:339451240093:web:d7aa50a60047e22c4***",
+  measurementId: "****"
 };
 
 
